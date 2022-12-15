8600b4e58195505675f2a155f8a53495b1c7c303
Armando Andini <armando.andini@gmail.com>
2022-12-15 10:23:15 -0300

    wip

---
+++ coc/src/index.ts
@@ -0,0 +1,18 @@
+import { ExtensionContext, LanguageClient, services } from "coc.nvim";
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  const languageClient = new LanguageClient(
+    "solidity",
+    "Solidity Language Server",
+    {
+      module: require.resolve("solidity-ls-antico"),
+      // module: require("path").join(__dirname, "..", "..", "solidity-ls"),
+      // options: { execArgv: ["--inspect"] },
+    },
+    {
+      documentSelector: ["solidity"],
+      synchronize: { configurationSection: "solidity" },
+    }
+  );
+  context.subscriptions.push(services.registLanguageClient(languageClient));
+}
